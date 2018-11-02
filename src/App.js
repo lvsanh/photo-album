23d9d94ff7326c615269ea3fb7755089932ea706
lvsanh <lvsanh@dtvt.org>
2018-11-02 10:01:00 +0100

    photolist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Provider} from 'react-redux'
+import store from './store'
 
 class App extends Component {
   render() {
